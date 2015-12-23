debb9a1143afc823ed0baea0a4079f952d980ad1
jhaowei-huang <jhaowei.huang@gmail.com>
2015-12-23 12:08:02 +0800

    init commit

---
+++ app/src/main/java/com/app/jhaowei/mp3player/Playing.java
@@ -0,0 +1,24 @@
+package com.app.jhaowei.mp3player;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.jhaowei.mp3player.EventHandler;
+
+public class Playing extends Fragment {
+	public static View rootView = null;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {        
+        return rootView = inflater.inflate(R.layout.playing_frag, container, false);
+    }
+    
+    @Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		EventHandler.setRelativeView(rootView);
+	}
+}
